7a1cd6032ed6579b871b4334b404d0b6288a6411
brenda0323manon <brendamanon0526@gmail.com>
2024-02-12 11:10:10 -0500

    edited

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './QuizPage.css';
 import { motion as m } from 'framer-motion';
